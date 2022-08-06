a450ed21866306ee517f6a9d424bca1f1a50c20c
さくや <shuoyeshantian@gmail.com>
2022-08-06 22:24:15 +0900

    ファイルの分割

--- setup.py
+++ setup.py
@@ -1,18 +1,55 @@
-import sqlite3
+from email.policy import default
+import os
+from unicodedata import category 
+from flask import Flask, render_template
+from flask import request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user,logout_user,login_required
+from sqlalchemy import column
+from sqlalchemy.dialects import postgresql as pg
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 
-dbname = '3y1w'
-conn = sqlite3.connect(dbname)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://3y1w.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = os.urandom(24)
 
-cur = conn.cursor()
+db = SQLAlchemy(app)
 
-cur.execute('create table USER(USER_ID integer auto_increment not null primary key, USER_NAME varchar(64), PASSWORD varchar(128) not null, EMAIL varchar(128));')
-cur.execute('create table CATEGORY(CATEGORY_ID integer auto_increment not null primary key, CATEGORY varchar(64) not null);')
-cur.execute('create table EVENT(EVENT_ID integer auto_increment not null primary key, EVENT_NAME varchar(64) not null);')
-cur.execute('create table DURING(DURING_ID integer auto_increment not null, EVENT_ID integer not null, DATE datetime not null, foreign key (EVENT_ID) references EVENT(EVENT_ID), primary key (DURING_ID, EVENT_ID));')
-cur.execute('create table EVENT_CATEGORY(EVENT_CATEGORY_ID integer auto_increment not null, EVENT_ID integer not null, CATEGORY_ID integer not null, foreign key (EVENT_ID) references EVENT(EVENT_ID), foreign key (CATEGORY_ID) references CATEGORY(CATEGORY_ID), primary key (EVENT_CATEGORY_ID, EVENT_ID));')
-cur.execute('create table USER_CATEGORY(USER_CATEGORY_ID integer auto_increment not null, USER_ID integer not null, EVENT_ID integer not null, CATEGORY_ID integer not null, foreign key (USER_ID) references USER(USER_ID), foreign key (EVENT_ID) references EVENT(EVENT_ID), foreign key (CATEGORY_ID) references CATEGORY(CATEGORY_ID), primary key (USER_CATEGORY_ID, USER_ID, EVENT_ID));')
+class USER(UserMixin, db.Model):
+    USER_ID   = db.Column(db.Integer, primary_key=True)
+    USER_NAME = db.Column(db.VarChar(56),nullable=False)
+    PASSWORD  = db.Column(db.VarChar(128))
+    EMAIL     = db.Column(db.VarChar(128))
 
-conn.commit()
+class CATEGORY():
+    CATEGORY_ID   = db.Column(db.Integer,    nullable=False, primary_key=True, autoincrement=True)
+    CATEGORY_NAME = db.Column(db.VarChar(64),nullable=False)
+    
+class EVENT(db.Model):
+    EVENT_ID   = db.Column(db.Integer,    nullable=False, autoincrement=True)
+    EVENT_NAME = db.Column(db.VarChar(64),nullable=False)
 
-cur.close()
-conn.close()
+class DURING(db.Model):
+    DURING_ID = db.Column(db.Integer,  nullable=False)
+    EVENT_ID  = db.Column(db.Integer,  nullable=False)
+    DATE      = db.Column(db.DateTime, nullable=False)
+
+class EVENT_CATEGORY(db.Model):
+    EVENT_CATEGORY_ID = db.Column(db.Integer, nullable=False)
+    EVENT_ID          = db.Column(db.Integer, nullable=False, autoincrement=True)
+    CATEGORY_ID       = db.Column(db.Integer, nullable=False)
+    
+class USER_CATEGORY(db.Model):
+    USER_CATEGORY_ID = db.Column(db.Integer, nullable=False, autoincrement=True)
+    USER_ID          = db.Column(db.Integer, nullable=False)
+    EVENT_ID         = db.Column(db.Integer, nullable=False)
+    CATEGORY_ID      = db.Column(db.Integer, nullable=False)
+
+class USER_INPUT(db.Model):
+    USER_ID       = db.Column(db.Integer,     primary_key=True)
+    EVENT_ID      = db.Column(db.Integer,     nullable=False, unique=True)
+    CATEGORY_ID   = db.Column(db.Integer,     nullable=False)
+    CATEGORY_NAME = db.Column(db.VarChar(64), nullable=False)
